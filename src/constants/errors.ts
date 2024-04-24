46ef9b5dcf620284230f3505e24584b0ea5447ae
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-25 00:08:49 +0200

    feat(network): add network errors strings

---
+++ src/constants/errors.ts
@@ -0,0 +1,14 @@
+export const errors = {
+  networkError: 'Please check your internet connection and try again',
+  requestTimeout: "Your internet connection isn't stable",
+  serverError: 'Server Error. Please delete and reinstall the App.',
+  generalError: 'General Error. Please delete and reinstall the App.',
+  unauthorizedError: "You aren't authorized, please re-login and check again",
+  forbiddenError: "You don't have access to perform this request",
+  rejectError: "You aren't authorized to perform this request",
+  resourceNotFound: 'Resource does not exist',
+  badDataError:
+    'Something went wrong when parsing data, if app is not responding, please delete and reinstall the App',
+  exceptionError:
+    'Something went wrong when performing request, if app is not responding, please delete and reinstall the App',
+};
