e58d31cf32b6fcb3d1f0c46bf108ab3eefb1585b
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-24 20:26:43 +0200

    feat: add empty screens to News Navigator

--- src/navigators/NewsNavigator/NewsFeedNavigator.tsx
+++ src/navigators/NewsNavigator/NewsFeedNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {IAppParamList} from '../AppNavigator/AppNavigator.types';
 import {AppRoutes} from '../AppNavigator/routes';
+import {NewsFeed, NewFeed} from '../../screens';
 
 const NewsFeedStack = createNativeStackNavigator<IAppParamList>();
 const NewsFeedNavigator = () => {
@@ -9,8 +10,8 @@ const NewsFeedNavigator = () => {
     <NewsFeedStack.Navigator
       initialRouteName={AppRoutes.NewsFeed}
       screenOptions={{headerShown: false}}>
-      <NewsFeedStack.Screen name={AppRoutes.NewsFeed} component={() => null} />
-      <NewsFeedStack.Screen name={AppRoutes.NewFeed} component={() => null} />
+      <NewsFeedStack.Screen name={AppRoutes.NewsFeed} component={NewsFeed} />
+      <NewsFeedStack.Screen name={AppRoutes.NewFeed} component={NewFeed} />
     </NewsFeedStack.Navigator>
   );
 };
