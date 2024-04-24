4c1ae1029632a09d4fc92b91c768399513826014
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-24 20:54:59 +0200

    feat(util): add ExtractProps type util

---
+++ src/utils/types.ts
@@ -0,0 +1,5 @@
+// Useful to extract props from styled components
+export type ExtractProps<TComponentOrTProps> =
+  TComponentOrTProps extends React.ComponentType<infer TProps>
+    ? TProps
+    : TComponentOrTProps;
