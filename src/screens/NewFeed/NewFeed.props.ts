840c300602f9a249b21c2f9e1cebfc7cfa9d68ee
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-25 04:18:35 +0200

    feat(screens): add NewFeed Screen Content

---
+++ src/screens/NewFeed/NewFeed.props.ts
@@ -0,0 +1,4 @@
+import {AppRoutes} from '../../navigators/AppNavigator/routes';
+import {INewsFeedRouteProp} from '../../navigators/NewsNavigator/NewsFeedNavigator.props';
+
+export type INewsFeedRoutePropRouteProp = INewsFeedRouteProp<AppRoutes.NewFeed>;
--- src/screens/NewFeed/NewFeed.tsx
+++ src/screens/NewFeed/NewFeed.tsx
@@ -1,10 +1,18 @@
-import {Text, View} from 'react-native';
 import React from 'react';
+import {Page} from '../../components/atoms';
+import {Text} from 'react-native';
+import {useRoute} from '@react-navigation/native';
+import {INewsFeedRoutePropRouteProp} from './NewFeed.props';
+import {NavigationHeader} from '../../components/atoms/NavigationHeader';
+import {NewsCardDetails} from '../../components/organisms';
 
 export default function NewFeed() {
+  const {params} = useRoute<INewsFeedRoutePropRouteProp>();
+  const {article} = params || {};
   return (
-    <View>
-      <Text>NewFeed</Text>
-    </View>
+    <Page>
+      <NavigationHeader headerTitle={article.title} />
+      <NewsCardDetails article={article} />
+    </Page>
   );
 }
