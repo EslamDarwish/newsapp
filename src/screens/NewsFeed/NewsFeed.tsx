299b00f98c11c9491c0970d8c546c143981ccd76
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-25 03:03:48 +0200

    fix(screens): fix flatlist issue

--- src/screens/NewsFeed/NewsFeed.tsx
+++ src/screens/NewsFeed/NewsFeed.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import useNews from '../../data/useNews';
 import {Page} from '../../components/atoms';
 import {FlatList} from 'react-native-gesture-handler';
 import {NewsCard} from '../../components/organisms/NewsCard';
 import {random} from 'lodash';
 import {NewsPageContainer} from './NewsFeed.styles';
+import {IArticle} from '../../models/article';
 
 export default function NewsFeed() {
   const {articles} = useNews();
 
+  const renderArticle = useCallback(
+    ({item}: {item: IArticle}) => <NewsCard article={item} />,
+    [],
+  );
   return (
     <Page>
       <NewsPageContainer>
         <FlatList
+          showsVerticalScrollIndicator={false}
           data={articles}
-          renderItem={({item}) => <NewsCard article={item} />}
-          keyExtractor={item => random(10000).toString() + item.title}
+          renderItem={renderArticle}
+          keyExtractor={item => item.source.id + item.title}
         />
       </NewsPageContainer>
     </Page>
