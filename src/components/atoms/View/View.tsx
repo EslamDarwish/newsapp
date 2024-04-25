266ae45f5fcb2432b3593d55673a1af4729de839
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-25 02:48:41 +0200

    fix(atoms): fix View Atom

--- src/components/atoms/View/View.tsx
+++ src/components/atoms/View/View.tsx
@@ -3,6 +3,6 @@ import {IViewProps} from './View.types';
 import {StyledView} from './View.styles';
 
 export const View = (props: IViewProps) => {
-  return <StyledView>{props.children}</StyledView>;
+  return <StyledView {...props} />;
 };
 export default View;
