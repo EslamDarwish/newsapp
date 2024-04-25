e53de149bac29450f6a04c75ad84d0c488d70cd6
Eslam Darwish <eslam.darwish.hassan@gmail.com>
2024-04-25 02:50:50 +0200

    feat(screens): add NewsCard to NewsFeed

---
+++ src/screens/NewsFeed/NewsFeed.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import {View} from '../../components/atoms';
+import {spacing} from '../../constants';
+
+export const NewsPageContainer = styled(View)`
+  flex: 1;
+  padding-horizontal: ${spacing.md}px;
+`;
--- src/screens/NewsFeed/NewsFeed.tsx
+++ src/screens/NewsFeed/NewsFeed.tsx
@@ -1,14 +1,23 @@
-import {Text, View} from 'react-native';
 import React from 'react';
 import useNews from '../../data/useNews';
 import {Page} from '../../components/atoms';
+import {FlatList} from 'react-native-gesture-handler';
+import {NewsCard} from '../../components/organisms/NewsCard';
+import {random} from 'lodash';
+import {NewsPageContainer} from './NewsFeed.styles';
 
 export default function NewsFeed() {
   const {articles} = useNews();
 
   return (
     <Page>
-      <Text>{JSON.stringify(articles)}</Text>
+      <NewsPageContainer>
+        <FlatList
+          data={articles}
+          renderItem={({item}) => <NewsCard article={item} />}
+          keyExtractor={item => random(10000).toString() + item.title}
+        />
+      </NewsPageContainer>
     </Page>
   );
 }
